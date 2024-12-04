e8aa296a0f66b97bafe29716e53eab0561e8b1c9
alisameteker3 <samet_ali_@hotmail.com>
2024-12-04 11:36:28 +0300

    Simple_Social_Media_App_with_Firebase

---
+++ URLShortener/URLShortener/app/src/main/java/com/ali/urlshortener/ApiServiceInterface.java
@@ -0,0 +1,12 @@
+package com.ali.urlshortener;
+
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.POST;
+
+public interface ApiServiceInterface {
+
+    @POST("shorten")
+    Call<ShortenResponse> shortenUrl(@Body ShortenRequest request);
+}
